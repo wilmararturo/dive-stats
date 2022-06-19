512209463c60f336e9c53cc045ae60ec78bdbf29
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-06-19 15:57:14 -0400

    added model and POST route for new divess #5

--- controllers/api/dives.js
+++ controllers/api/dives.js
@@ -15,4 +15,13 @@ router.get("/stats", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { rows } = await Dive.create(req.body);
+    res.json(rows[0]);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
