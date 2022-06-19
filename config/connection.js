7e7161e64b23307336c3671aefbaaa9cc2a1f9de
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-06-19 01:27:55 -0400

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params) => {
+    return pool.query(text, params);
+  },
+};
